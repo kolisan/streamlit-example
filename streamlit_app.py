e59a9c3a3c5210ac3a854e55f99e0812e13c8eff
kolisan <109546865+kolisan@users.noreply.github.com>
2023-02-02 12:04:51 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import math
 import pandas as pd
 import streamlit as st
 
-df = pd.DataFrame(['Teste1', 'Teste2'], columns  = ['Teste'])
+df = pd.DataFrame([['Teste1', 'Teste2']], columns  = ['Teste'])
 st.write(df)
